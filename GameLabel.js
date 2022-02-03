e32ea6daf401c27c9b64408e03c433cc58cd461a
Alberto <aldo10012@gmail.com>
2022-02-03 09:19:03 -0800

    make GameLabel inherite from Sprite

--- GameLabel.js
+++ GameLabel.js
@@ -1,12 +1,14 @@
 // GameLabel.js
+import Sprite from './Sprite.js';
 
-class GameLabel {
+class GameLabel extends Sprite {
   constructor(text, value, x, y, color = '#0095DD', font = '16px Arial') {
+    super(x, y, 0, 0, color)
     this.text = text
     this.value = value;
-    this.x = x;
-    this.y = y;
-    this.color = color;
+    // this.x = x;
+    // this.y = y;
+    // this.color = color;
     this.font = font
   }
   
